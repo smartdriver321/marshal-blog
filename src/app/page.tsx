7996c583f511af98257b23453f9512f7ec1b75de
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-12 11:37:41 +0700

    Configure Kinde authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+	LoginLink,
+	LogoutLink,
+	RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
+
 import { Button } from '@/components/ui/button'
 
-export default function HomePage() {
+export default async function HomePage() {
+	const { getUser } = getKindeServerSession()
+	const session = await getUser()
+
 	return (
-		<div>
-			<Button className='text-red-500'>Click Me!</Button>
+		<div className='p10'>
+			<Button className='text-red-500'>Kinde Auth</Button>
+			{session ? (
+				<LogoutLink>
+					<Button>Logout</Button>{' '}
+				</LogoutLink>
+			) : (
+				<div>
+					<RegisterLink>
+						{' '}
+						<Button>Register</Button>
+					</RegisterLink>
+					<LoginLink>
+						<Button>Login</Button>{' '}
+					</LoginLink>
+				</div>
+			)}
 		</div>
 	)
 }

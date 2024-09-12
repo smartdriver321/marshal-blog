a999dbb899df0cef90f66bff9ebdd9c79a72ffed
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-12 15:28:14 +0700

    Create SitesPage

--- src/app/dashboard/sites/page.tsx
+++ src/app/dashboard/sites/page.tsx
@@ -1,3 +1,27 @@
-export default function Sitespage() {
-	return <div>Sitespage</div>
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import { PlusCircle } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+
+import { Button } from '@/components/ui/button'
+
+export default async function SitesPage() {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
+	if (!user) {
+		return redirect('/api/auth/login')
+	}
+
+	return (
+		<>
+			<div className='flex w-full justify-end'>
+				<Button asChild>
+					<Link href={'/dashboard/sites/new'}>
+						<PlusCircle className='mr-2 size-4' /> Create Site
+					</Link>
+				</Button>
+			</div>
+		</>
+	)
 }
